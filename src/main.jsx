cb5584e43c28b81b8eb33a735874c0d4856a4345
Ne0sky <99585944+Ne0sky@users.noreply.github.com>
2023-12-31 17:48:33 +0530

    create routes-> doctor, admin

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,11 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { AuthContextProvider } from './context/AuthContext.jsx'
-import { Outlet, RouterProvider, createBrowserRouter } from 'react-router-dom'
-import SigninForm from './components/SigninForm.jsx'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 import Layout from './Layout.jsx'
+import Landing from './pages/Landing.jsx'
+import Doctor from './pages/Doctor.jsx'
+import Admin from './pages/Admin.jsx'
 
 const routes = [
   {
@@ -14,8 +16,17 @@ const routes = [
     children: [
       {
         path: '',
-        element: <SigninForm/>,
-      }
+        element: <Landing/>,
+      },
+      {
+        path: 'admin',
+        element: <Admin/>
+      },
+      {
+        path: 'doctor',
+        element: <Doctor/>,
+      },
+      
     ],
   },
 ];
