589425c9a79f03bbd7739ecc45b65f693c8313cf
Ne0sky <99585944+Ne0sky@users.noreply.github.com>
2023-12-31 21:31:28 +0530

    delete cookie on logout fix

--- src/hooks/useLogout.jsx
+++ src/hooks/useLogout.jsx
@@ -1,7 +1,7 @@
 
 // import  toast  from 'react-hot-toast';
 import {useAuthContext} from './useAuthContext'
-
+import Cookies from "universal-cookie"
 import { useNavigate } from "react-router-dom";
 
 export const useLogout = () =>{
@@ -9,6 +9,8 @@ export const useLogout = () =>{
 const {dispatch} = useAuthContext()
     const logout=()=>{
         // remove user from local storage
+        const cookies = new Cookies();
+        cookies.remove('token')
         localStorage.removeItem('user');
         //dispatch logout action
         dispatch({type:'LOGOUT'})
